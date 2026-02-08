420d8cb91a60a86781c35733bce8f8548b8de69c
Draemiz <draemizgx1@gmail.com>
2026-02-08 00:11:54 -0500

    Implementa creación de empleados con validaciones y registro en tabla

--- roles/roles.js
+++ roles/roles.js
@@ -68,6 +68,81 @@ ejecutarNuevo = function(){
     esNuevo = true;
 }
 
+buscarEmpleado = function(cedula){
+    for(let i = 0; i < empleados.length; i++){
+        if(empleados[i].cedula === cedula){
+            return empleados[i];
+        }
+    }
+    return null;
+}
+
+agregarEmpleado = function(empleado){
+    let existe = buscarEmpleado(empleado.cedula);
+    if(existe == null){
+        empleados.push(empleado);
+        return true;
+    }
+    return false;
+}
+
+guardar = function(){
+
+    let cedula = recuperarTexto("txtCedula");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let sueldoTxt = recuperarTexto("txtSueldo");
+    let sueldo = parseFloat(sueldoTxt);
+
+    let valido = true;
+
+    // Limpiar los errores
+    mostrarTexto("lblErrorCedula", "");
+    mostrarTexto("lblErrorNombre", "");
+    mostrarTexto("lblErrorApellido", "");
+    mostrarTexto("lblErrorSueldo", "");
+
+    // validaciones para cedula, nombre, apelliod y sueldo 
+    if(cedula.length !== 10 || isNaN(cedula)){
+        mostrarTexto("lblErrorCedula", "Cédula inválida debe tener 10 caractéres");
+        valido = false;
+    }
+    if(nombre.length < 3 || nombre !== nombre.toUpperCase()){
+        mostrarTexto("lblErrorNombre", "Nombre inválido, debe ir todo en mayúsculas");
+        valido = false;
+    }
+    if(apellido.length < 3 || apellido !== apellido.toUpperCase()){
+        mostrarTexto("lblErrorApellido", "Apellido inválido, debe ir todo en mayúsculas");
+        valido = false;
+    }
+    if(isNaN(sueldo) || sueldo < 400 || sueldo > 5000){
+        mostrarTexto("lblErrorSueldo", "Sueldo inválido min $400, máx 5000");
+        valido = false;
+    }
+
+    if(!valido){
+        return;
+    }
+    if(esNuevo){
+        let emp = {};
+        emp.cedula = cedula;
+        emp.nombre = nombre;
+        emp.apellido = apellido;
+        emp.sueldo = sueldo;
+
+        let resultado = agregarEmpleado(emp);
+
+        if(resultado){
+            alert("EMPLEADO GUARDADO CORRECTAMENTE");
+            mostrarEmpleados();
+            deshabilitarFormularioEmpleado();
+            esNuevo = false;
+        }else{
+            alert("YA EXISTE UN EMPLEADO CON LA CEDULA " + cedula);
+        }
+    }
+}
+
 
 deshabilitarFormularioEmpleado = function(){
     deshabilitarComponente("txtCedula");
